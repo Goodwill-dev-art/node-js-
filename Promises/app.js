64b0f798582b80bc4158b6f045764788139992ea
Goodwill-dev-art <oriakhig@gmail.com>
2026-02-04 08:36:54 +0100

    converting callback hell to promises

--- Promises/app.js
+++ Promises/app.js
@@ -1,19 +1,28 @@
-// problem with call back hell
-// - its make code difficult to maintain
-// it is easier to identify with the trangle shape in the code  which indicate that its a call back hell which means callback inside of another callback from the example below
-const fs = require("fs");
-const superagent = require("superagent");
-fs.readFile("dog.txt", "utf-8", (err, data) => {
-  console.log(`breed : ${data}`);
 
-  // second callBack
-  superagent.get(`https://dog.ceo/api/breed/${data}/images/random`).end((err, res) => {
-    console.log(res.body.message);
+ // callback hell
+ 
+ //  the "get"  in the superagent module will return a primise
+ // after which we can consume with the then methid
+ // when a promise successfully return a value is a resolved promised which is fulfled
+ // using the catch method to handle resolve promises
+ 
+ const fs = require("fs");
 
-    fs.writeFile("breed-image.txt", res.body.message, (err, data) => {
-      if (err) return err.message;
-    });
-  });
-});
-// callback hell
+ const superagent = require("superagent");
+ fs.readFile("dog.txt", "utf8", (err, data) => {
+   console.log(`breed : ${data}`);
+ 
+
+   superagent
+     .get(`https://dog.ceo/api/breed/${data}/images/random`)
+     .then((res) => {
+       console.log(res.body.message);
+ 
+       fs.writeFile("breed-image.txt", res.body.message, (err, data) => {
+         if (err) return console.log(err.message);
+         console.log("random dog image has been saved ")
+       });
+     })
+     .catch((err) => console.log(err.message));
+ });
  
