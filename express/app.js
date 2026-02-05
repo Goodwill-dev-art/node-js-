77faa9ce926ca3562c6bd47dfc8b772f7b26a65f
Goodwill-dev-art <oriakhig@gmail.com>
2026-02-05 11:06:27 +0100

    What is Express

---
+++ express/app.js
@@ -0,0 +1,3 @@
+// express is a minimal nodejs framework , a higher level of abstracton
+// it allows rapid developnent of Node.js  application
+// contains a very robust set of features : complex routing esier handling of request and responses, middleware server side rending 
